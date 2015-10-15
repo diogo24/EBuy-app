5c345d1752b36dfdaec196c25bd783415c826973
Diogo24 <diocar24@hotmail.com>
2015-10-15 17:10:47 +0100

    1) Add AuctionsController 2) Add Details View to Auctions 3) Add EBuy.Api project

---
+++ EBuy/EBuy.Api/Models/Auction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EBuy.Api
+{
+    public class Auction
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal StartPrice { get; set; }
+        public decimal CurrentPrice { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
---
+++ EBuy/EBuy/Controllers/EBuy/AuctionsController.cs
@@ -0,0 +1,104 @@
+﻿using EBuy.Api;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EBuy.Controllers.EBuy
+{
+    public class AuctionsController : Controller
+    {
+        // GET: Auctions
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Auctions/Details/5
+        public ActionResult Details(long id)
+        {
+            CultureInfo usCulture = new CultureInfo("en-US");
+
+            var auction = new Auction
+            {
+                Id           = id,
+                Title        = "Brand new Widget 2.0",
+                Description  = "This is a brand new version 2.0 Widget!",
+                StartPrice   = 1.00m,
+                CurrentPrice = 13.40m,
+                StartTime    = DateTime.Parse("6-15-2012 12:34 PM", usCulture.DateTimeFormat),
+                EndTime      = DateTime.Parse("6-23-2012 12:34 PM", usCulture.DateTimeFormat),
+            };
+
+            return View(auction);
+        }
+
+        // GET: Auctions/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Auctions/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Auctions/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Auctions/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Auctions/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Auctions/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
