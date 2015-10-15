bd183fb70e7768aa2e673ea309af263471813b6a
Diogo24 <diocar24@hotmail.com>
2015-10-15 13:32:51 +0100

    Layouts and render sections

---
+++ EBuy/EBuy/Controllers/TestLayoutController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EBuy.Controllers
+{
+    public class TestLayoutController : Controller
+    {
+        // GET: TestLayout
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
