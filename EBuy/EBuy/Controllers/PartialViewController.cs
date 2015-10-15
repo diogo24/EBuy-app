f2b9e8216ede780eb21bbd0fb624fb14bc4c4bb6
Diogo24 <diocar24@hotmail.com>
2015-10-15 13:47:05 +0100

    Partial view - used for reuse ui elements.

---
+++ EBuy/EBuy/Controllers/PartialViewController.cs
@@ -0,0 +1,33 @@
+﻿using EBuy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EBuy.Controllers
+{
+    public class PartialViewController : Controller
+    {
+        // GET: PartialView
+        public ActionResult Index()
+        {
+            var partialViewVmList = GetPartialViewVMList();
+
+            return View(partialViewVmList);
+        }
+
+        #region Private Members 
+
+        private IEnumerable<PartialViewVM> GetPartialViewVMList()
+        {
+            return new List<PartialViewVM> {
+                new PartialViewVM { Name = "Name1", Value = 1},
+                new PartialViewVM { Name = "Name2", Value = 2},
+                new PartialViewVM { Name = "Name3", Value = 3}
+            };
+        }
+
+        #endregion
+    }
+}
---
+++ EBuy/EBuy/Models/PartialViewVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EBuy.Models
+{
+    public class PartialViewVM
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+    }
+}
