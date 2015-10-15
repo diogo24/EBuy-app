844e2896b9ad233202920d8b11655d82256fe180
Diogo24 <diocar24@hotmail.com>
2015-10-15 12:46:53 +0100

    EBuy solution

---
+++ EBuy/EBuy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EBuy.Startup))]
+namespace EBuy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
