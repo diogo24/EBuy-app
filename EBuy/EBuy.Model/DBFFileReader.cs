7e09f1f9f12834d712935e484bb163976e4077a2
Diogo24 <diocar24@hotmail.com>
2015-10-20 19:47:41 +0100

    Read table from Cumas.dbf file

---
+++ EBuy/EBuy.Model/DBFFileReader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EBuy.Model
+{
+    public class DBFFileReader
+    {
+        public DataTable GetCumasFileData()
+        {
+            DataTable resultData = new DataTable();
+
+            using (OleDbConnection connectionHandler = new OleDbConnection(
+            @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\diogo.marques\Documents\GitHubVisualStudio\EBuy\EBuy\EBuy.Model\Files\;Extended Properties=dBASE IV"))
+            {
+                // Open the connection, and if open successfully, you can try to query it
+                connectionHandler.Open();
+
+                if (connectionHandler.State == ConnectionState.Open)
+                {
+                    string mySQL = "select * from Cumas";  // dbf table name
+
+                    OleDbCommand MyQuery = new OleDbCommand(mySQL, connectionHandler);
+                    OleDbDataAdapter DA  = new OleDbDataAdapter(MyQuery);
+
+                    DA.Fill(resultData);
+
+                    connectionHandler.Close();
+                }
+
+                return resultData;
+            }
+        }
+    }
+}
---
+++ EBuy/EBuy/Controllers/Salonman/CumasController.cs
@@ -0,0 +1,21 @@
+﻿using EBuy.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EBuy.Controllers.Salonman
+{
+    public class CumasController : Controller
+    {
+        // GET: Cumas
+        public ActionResult Index()
+        {
+            var dbfFileReader = new DBFFileReader();
+            var data          = dbfFileReader.GetCumasFileData();
+
+            return View(data);
+        }
+    }
+}
