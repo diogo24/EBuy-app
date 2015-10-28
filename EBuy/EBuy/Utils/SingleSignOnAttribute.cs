7cec69f54cec7879de2626664c055ffad0e8cd16
Diogo24 <diocar24@hotmail.com>
2015-10-28 13:09:54 +0000

    SingleSignOnAttribute: SingleSignOnAttribute is an example of a custom ActionFilter that has been created for supporting Single Sign On Authentication across multiple ASP.NET Web applications

---
+++ EBuy/EBuy/Utils/SingleSignOnAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EBuy.Utils
+{
+    public class SingleSignOnAttribute : ActionFilterAttribute, IActionFilter
+    {
+        void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            // Verify security token and authenticate user
+        }
+        void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+        // Preprocessing code used to verify if security token exists
+        }
+    }
+}
