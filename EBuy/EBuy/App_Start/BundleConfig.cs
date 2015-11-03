0cab1759828226b9d5f08943e6adc5508f5d9fcc
Diogo24 <diocar24@hotmail.com>
2015-11-03 18:32:47 +0000

    1) Install Foundation5.MVC.Sass nuget package. 2) Install Web Essentials http://vswebessentials.com/download

--- EBuy/EBuy/App_Start/BundleConfig.cs
+++ EBuy/EBuy/App_Start/BundleConfig.cs
@@ -1,4 +1,4 @@
-﻿using System.Web;
+using System.Web;
 using System.Web.Optimization;
 
 namespace EBuy
@@ -25,7 +25,15 @@ namespace EBuy
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/SiteCommun.css"));
+
+            bundles.Add(new StyleBundle("~/Content/Foundation/css").Include(
+                      "~/Content/Site.css"));
+
+            #region Foundation Bundles
+
+            bundles.Add(Foundation.Scripts());
+            #endregion
         }
     }
 }
--- EBuy/EBuy/Controllers/HomeController.cs
+++ EBuy/EBuy/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace EBuy.Controllers
             return View();
         }
 
+        public ActionResult IndexFoundation()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
